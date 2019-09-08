d75a3357e5d982973c84dc58fb1bd30f756b5239
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 21:44:06 +0800

    SHup20190908 Version = 17

--- slide/OPAandPCB/up-1.sh
+++ slide/OPAandPCB/up-1.sh
@@ -2,18 +2,15 @@
 echo "UP!"
 file='Version.txt'
 ( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
-ver=`cat $file`
-echo previous ver. = ${ver}
-ver=`expr $ver + 1`
-echo current ver. = $ver
 echo `expr $(cat $file) + 1` > ${file}
+echo Current Version = `cat < ${flie}`
 cd ~/website/uebusaito
 date=$(date +%Y%m%d)
 preMSG="SHup"
 strVER=' Version = '
-mesg=${preMSG}${date}${strVER}${ver}
+mesg=${preMSG}${date}${strVER}`expr $(cat $file) + 0`
 git init
 git add -A
 git commit -m "${mesg}"
 git push -u origin gh-pages
-exit 0        
+exit 0
