c366a89934e43f8c183a14885eb5633f443bbc0e
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 18:46:07 +0800

    SHup20190908

--- slide/OPAandPCB/up.sh
+++ slide/OPAandPCB/up.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "UP!"
 cd ~/website/uebusaito
+date=$(date +%Y%m%d)
+preMSG="SHup"
+mesg=${preMSG}${date}
 git init
 git add -A
-git commit -m "UPSH"
+git commit -m "${mesg}"
 git push -u origin gh-pages
 exit 0
