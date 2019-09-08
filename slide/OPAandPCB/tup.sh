c32082a02b60d6c17b050f519b8838605a45b221
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 21:13:51 +0800

    SHup20190908 Version = Version.txt

--- slide/OPAandPCB/tup.sh
+++ slide/OPAandPCB/tup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "UP!"
 file='Version.txt'
-( [ -e "$file" ] || touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) )
-ver=0
-cat $file > $ver 
+( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
+ver=`cat $file`
 echo ver= ${ver}
 ver=`expr $ver + 1`
 echo ver+1= $ver
-
+echo `expr $(cat $file) + 1` > ${file}
--- slide/OPAandPCB/up.sh
+++ slide/OPAandPCB/up.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 echo "UP!"
 file='Version.txt'
-( [ -e "$file" ] || touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) )
-ver=0
-cat $file > $ver
-verP=`expr $ver + 1`
-echo ${verP} > ${file}
-echo 'Version change from' $ver 'to' $verP
+( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
+echo `expr $(cat $file) + 1` > $file
+echo 'Current Version =' $flie
 cd ~/website/uebusaito
 date=$(date +%Y%m%d)
 preMSG="SHup"
 strVER=' Version = '
-mesg=${preMSG}${date}${strVER}${verP}
+mesg=${preMSG}${date}${strVER}${file}
 git init
 git add -A
 git commit -m "${mesg}"
