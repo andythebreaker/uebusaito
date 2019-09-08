961865e50e526f3acf943b8b531f79aa5ec3ee57
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 21:32:43 +0800

    SHup20190908 Version = 0

--- slide/OPAandPCB/up.sh
+++ slide/OPAandPCB/up.sh
@@ -2,7 +2,7 @@
 echo "UP!"
 file='Version.txt'
 ( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
-echo `expr $(cat $file) + 1` > $file
+echo `expr $(cat $file) + 1` > ${file}
 echo Current Version = `cat < ${flie}`
 cd ~/website/uebusaito
 date=$(date +%Y%m%d)
