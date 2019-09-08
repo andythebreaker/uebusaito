ca477687f881272d643ed24ea8205f29d9049e4d
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 21:43:08 +0800

    SHup20190908 Version = 16

--- slide/OPAandPCB/cup.sh
+++ slide/OPAandPCB/cup.sh
@@ -3,9 +3,9 @@ echo "UP!"
 file='Version.txt'
 ( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
 ver=`cat $file`
-echo ver= ${ver}
+echo previous ver. = ${ver}
 ver=`expr $ver + 1`
-echo ver+1= $ver
+echo current ver. = $ver
 echo `expr $(cat $file) + 1` > ${file}
 cd ~/website/uebusaito
 date=$(date +%Y%m%d)
