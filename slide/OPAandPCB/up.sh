2ad85f45c18bcc60602d04eaff388da56eebb5fc
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-09-08 21:19:45 +0800

    SHup20190908 Version =

--- slide/OPAandPCB/up.sh
+++ slide/OPAandPCB/up.sh
@@ -3,12 +3,12 @@ echo "UP!"
 file='Version.txt'
 ( ( [ -e "$file" ] ) && ( ( [ ! -w "$file" ] && echo Ver~.txt EXIT but cannot write ) || echo file Ver~.txt checked ! ) ) || ( ( touch "$file" ) && ( ( ( [ ! -w "$file" ] ) && ( echo cannot write to $file ) ) || ( ( echo "0" > ${file} ) && ( echo writed $file ) ) ) )
 echo `expr $(cat $file) + 1` > $file
-echo 'Current Version =' $flie
+echo 'Current Version =' `echo cat $flie`
 cd ~/website/uebusaito
 date=$(date +%Y%m%d)
 preMSG="SHup"
 strVER=' Version = '
-mesg=${preMSG}${date}${strVER}${file}
+mesg=${preMSG}${date}${strVER}`$(cat $file)`
 git init
 git add -A
 git commit -m "${mesg}"
