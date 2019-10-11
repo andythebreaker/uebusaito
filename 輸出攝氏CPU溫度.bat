fe23ee4234409ad76dee3739a70184eeb2dc70f7
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-11 21:43:26 +0800

    Add files via upload

---
+++ 輸出攝氏CPU溫度.bat
@@ -0,0 +1,28 @@
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
+for /f "delims== tokens=2" %%a in (
+    'wmic /namespace:\\root\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature /value'
+) do (
+    set /a degrees_celsius=%%a / 10 - 273
+)
+
+echo %degrees_celsius%
+pause
