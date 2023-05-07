40b48546e8b069e8e5eb1ba3d331c8b47e63169a
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2023-05-08 01:32:46 +0800

    Update hct.m

--- hct.m
+++ hct.m
@@ -1,18 +1,18 @@
 t0 = clock;
+% Parameter Setting
+N=3;
+M=224;
+L=10000;
+
 % Algorithm
-%load Nevada
-X3D=X;
-whz=size(X3D);
-M=whz(3);
-L=whz(1)*whz(2);
+load data30
 X = reshape(X3D,L,M)'; % 3D to 2D
 
 %Generate missing data(Revise Here)
 Y = X;
-pixelcut = [1:600 ];%, 1767:2500 , 2710:2722 , 2800:3500 , 3520 , 3545:3596 , 3600 , 3710 , 3800:10000]; 
-bandcut = [1:2 ];%, 223:224];                                                                           
+pixelcut = [1:1702 , 1767:2500 , 2710:2722 , 2800:3500 , 3520 , 3545:3596 , 3600 , 3710 , 3800:10000]; 
+bandcut = [1:220 , 223:224];                                                                           
 Y(bandcut , pixelcut) =0;
-%showrgbhyper_2d(Y,N);
 
 % Subset 
 Y_Omega = X;
@@ -20,7 +20,7 @@ Y_Omega(: , pixelcut) = [];
  
 % Obtain A by HyperCSI
 [~ , L] = size(Y_Omega);        
-[A, timeHyperCSI,a] = hpycsi(Y_Omega,N,false);
+[A, timeHyperCSI,a] = HyperCSI(Y_Omega,N);
 
 % Subset
 A_Omega = A;    
@@ -43,27 +43,35 @@ Y_Ans = A*S;
 
 % Performance Comparison
 time = etime(clock,t0);
-percentage = Missing_Percentage(Y);
-Y1 = reshape(Y_Ans',whz(1),whz(2),M);
-error = frobenius_norm(Y1,X3D);
+percentage = Missing_Percentage(Y)
+Y1 = reshape(Y_Ans',100,100,224);
+error = Error_Calculation(Y1,X3D)
 
-pa=percentage;
-er=error;
+% plot
+%  x = a(1,:); y = a(2,:);
+%  scatter(x,y,'filled');
 
-%% plt
-if(pltendbool==true)
 figure;
-for i = 1:N
-    map_est = reshape(S(i,:), whz(1), whz(2));
-    subplot(2, N, i);
-    imshow(map_est);
-    title(['map ' num2str(i) ' est']);
-end
+map1_est= reshape(S(1,:),100,100);
+subplot(2,3,1);
+imshow(map1_est);title('map 1 est');
 
-for i = 1:N
-    subplot(2, N, i+N);
-    plot(A(:,i));
-    title(['est signature ' num2str(i)]);
-    axis([1 M 0 1]);
-end
-end
+map2_est= reshape(S(2,:),100,100);
+subplot(2,3,2);
+imshow(map2_est);title('map 2 est');
+ 
+map3_est= reshape(S(3,:),100,100);
+subplot(2,3,3);
+imshow(map3_est);title('map 3 est');
+ 
+subplot(2,3,4);
+plot(A(:,1)); title('est signature 1');
+axis([1 224 0 1]);
+
+subplot(2,3,5);
+plot(A(:,2)); title('est signature 2');
+axis([1 224 0 1]);
+
+subplot(2,3,6);
+plot(A(:,3)); title('est signature 3');
+axis([1 224 0 1]);
